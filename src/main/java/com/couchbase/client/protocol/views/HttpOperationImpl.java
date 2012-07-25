228b3cd3c3d1d171779ccbdb10bbd3974f8a8a57
Matt Ingenthron <ingenthr@cep.net>
2012-07-25 01:40:43 -0700

    Added better debug logging on ViewNode error handling.
    Change-Id: Ic1ee33df0a95ce285143269b9cc1246cc8be0913
    Reviewed-on: http://review.couchbase.org/18808
    Reviewed-by: Matt Ingenthron <matt@couchbase.com>
    Tested-by: Matt Ingenthron <matt@couchbase.com>

--- src/main/java/com/couchbase/client/protocol/views/HttpOperationImpl.java
+++ src/main/java/com/couchbase/client/protocol/views/HttpOperationImpl.java
@@ -143,4 +143,22 @@ public abstract class HttpOperationImpl implements HttpOperation {
     return new OperationStatus(false, "Error Code: " + errorcode
         + "No entity");
   }
+
+  @Override
+  public String toString() {
+    StringBuilder rv = new StringBuilder(HttpOperationImpl.class.getName());
+    rv.append(" request:").append(getRequest())
+      .append(" has errored: ").append(hasErrored())
+      .append(" is  cancelled: ").append(isCancelled())
+      .append(" is timed out: ").append(isTimedOut());
+    if (null != getException()) {
+      rv.append(" with contained exception: ")
+        .append(getException().getClass().getName())
+        .append(": ").append(getException().getMessage());
+    }
+    return rv.toString();
+
+  }
+
+
 }
