c73d7fab29534f5148bea79eb8ff9f40b6f5facb
Mike Wiederhold <mike@couchbase.com>
2012-09-28 12:25:06 -0700

    JCBC-105: Add key stats operation
    Change-Id: I5d5300ae29cdc7d73477a488b4554126325213bc
    Reviewed-on: http://review.couchbase.org/21202
    Reviewed-by: Matt Ingenthron <matt@couchbase.com>
    Tested-by: Michael Wiederhold <mike@couchbase.com>

--- src/main/java/com/couchbase/client/CouchbaseClient.java
+++ src/main/java/com/couchbase/client/CouchbaseClient.java
@@ -83,6 +83,7 @@ import net.spy.memcached.ops.ObserveOperation;
 import net.spy.memcached.ops.Operation;
 import net.spy.memcached.ops.OperationCallback;
 import net.spy.memcached.ops.OperationStatus;
+import net.spy.memcached.ops.StatsOperation;
 import net.spy.memcached.transcoders.Transcoder;
 
 import org.apache.http.HttpRequest;
@@ -1211,5 +1212,28 @@ public class CouchbaseClient extends MemcachedClient
         throw new ObservedException("Observe was Interrupted ");
       }
     }
+  }
+
+  public OperationFuture<Map<String, String>> getKeyStats(String key) {
+    final CountDownLatch latch = new CountDownLatch(1);
+    final OperationFuture<Map<String, String>> rv =
+        new OperationFuture<Map<String, String>>(key, latch, operationTimeout);
+    Operation op = opFact.keyStats(key, new StatsOperation.Callback() {
+          Map<String, String> stats = new HashMap<String, String>();
+          public void gotStat(String name, String val) {
+            stats.put(name, val);
+          }
+
+          public void receivedStatus(OperationStatus status) {
+            rv.set(stats, status);
+          }
+
+          public void complete() {
+            latch.countDown();
+          }
+        });
+    rv.setOperation(op);
+    mconn.enqueueOperation(key, op);
+    return rv;
   }
 }
--- src/test/java/com/couchbase/client/CouchbaseClientTest.java
+++ src/test/java/com/couchbase/client/CouchbaseClientTest.java
@@ -192,6 +192,26 @@ public class CouchbaseClientTest extends BinaryClientTest {
             : "Key add was not persisted to master : "
             + addOp.getStatus().getMessage();
   }
+
+  public void testStatsKey() throws Exception {
+    client.set("key", 0, "value");
+    OperationFuture<Map<String, String>> future =
+        ((CouchbaseClient)client).getKeyStats("key");
+    assertTrue(future.getStatus().isSuccess());
+    Map<String, String> stats = future.get();
+    assertTrue(stats.size() == 7);
+    assertTrue(stats.containsKey("key_vb_state"));
+    assertTrue(stats.containsKey("key_flags"));
+    assertTrue(stats.containsKey("key_is_dirty"));
+    assertTrue(stats.containsKey("key_cas"));
+    assertTrue(stats.containsKey("key_data_age"));
+    assertTrue(stats.containsKey("key_exptime"));
+    assertTrue(stats.containsKey("key_last_modification_time"));
+
+    future = ((CouchbaseClient)client).getKeyStats("key1");
+    assertFalse(future.getStatus().isSuccess());
+  }
+
   public void testGetStatsSlabs() throws Exception {
     // Empty
   }
