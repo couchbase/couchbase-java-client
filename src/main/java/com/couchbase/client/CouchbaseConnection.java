ccde0d0738780b554cf6ad3c7ccea52eb00e24c8
Tugdual Grall <tugdual@gmail.com>
2012-12-08 17:44:19 +0100

    Small typo in exception
    Change-Id: I8b6dd24aa2614d5de57d4504625c72c1b304cffa
    Reviewed-on: http://review.couchbase.org/23157
    Reviewed-by: Michael Nitschinger <michael.nitschinger@couchbase.com>
    Tested-by: Michael Nitschinger <michael.nitschinger@couchbase.com>

--- src/main/java/com/couchbase/client/CouchbaseConnection.java
+++ src/main/java/com/couchbase/client/CouchbaseConnection.java
@@ -159,9 +159,9 @@ public class CouchbaseConnection extends MemcachedConnection  implements
       if (placeIn == null) {
         placeIn = primary;
         this.getLogger().warn(
-            "Node exepcted to receive data is inactive.  This could be due to "
-            + "a failure within the cluster.  Will check for updated "
-            + "configuration.  Key without a configured node is: %s.", key);
+            "Node expected to receive data is inactive. This could be due to "
+            + "a failure within the cluster. Will check for updated "
+            + "configuration. Key without a configured node is: %s.", key);
         cf.checkConfigUpdate();
       }
     }
