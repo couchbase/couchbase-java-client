68a348d993cadab9f8767e023ac908e7e0791020
Michael Nitschinger <michael@nitschinger.at>
2013-02-28 22:37:32 +0100

    Only check for replica if there really is one used.
    This changeset changes the behavior on how the code
    makes sure that there are enough replicas available
    in a way so that even if there is only a PersistTo
    constraint applied and no ReplicateTo, it will not
    fail with an IndexOutOfBound exception. If this is
    the case, then it is simply ignore and only the
    PersistTo logic is applied.
    
    Change-Id: Ibcfbe13cf0ee1ef791e6c3202723074b05d32833
    Reviewed-on: http://review.couchbase.org/24935
    Reviewed-by: Matt Ingenthron <matt@couchbase.com>
    Tested-by: Michael Nitschinger <michael.nitschinger@couchbase.com>

--- src/main/java/com/couchbase/client/CouchbaseClient.java
+++ src/main/java/com/couchbase/client/CouchbaseClient.java
@@ -1741,14 +1741,16 @@ public class CouchbaseClient extends MemcachedClient
     VBucketNodeLocator locator = ((VBucketNodeLocator)
         ((CouchbaseConnection) mconn).getLocator());
 
-    int vBucketIndex = locator.getVBucketIndex(key);
-    int currentReplicaNum = cfg.getReplica(vBucketIndex, numReplica-1);
-
-    if (currentReplicaNum < 0) {
-      throw new ObservedException("Currently, there is no replica available for"
-        + "the given replica index. This can be the case because of a failed "
-        + "over node which has not yet been rebalanced.");
+    if(numReplica > 0) {
+      int vBucketIndex = locator.getVBucketIndex(key);
+      int currentReplicaNum = cfg.getReplica(vBucketIndex, numReplica-1);
+      if (currentReplicaNum < 0) {
+        throw new ObservedException("Currently, there is no replica available for"
+          + "the given replica index. This can be the case because of a failed "
+          + "over node which has not yet been rebalanced.");
+      }
     }
+
 
     int replicaCount = Math.min(locator.getAll().size() - 1,
           cfg.getReplicasCount());
