4a35caf707c2df20e5d800470e5a7d1f49f17a02
Deepti Dawar <deepti.dawar@globallogic.com>
2013-04-09 15:26:53 +0530

    JCBC-282 Two tests added for connection refused and network un-reachable.
    Change-Id: I6bab9dd16a9804bfd4232c1433fc186028300703
    Reviewed-on: http://review.couchbase.org/25563
    Reviewed-by: Matt Ingenthron <matt@couchbase.com>
    Tested-by: Deepti Dawar <deepti.dawar@globallogic.com>

--- src/test/java/com/couchbase/client/ClusterManagerTest.java
+++ src/test/java/com/couchbase/client/ClusterManagerTest.java
@@ -311,6 +311,59 @@ public class ClusterManagerTest extends TestCase {
   }
 
   /**
+   * This test is performed by having the client connect to a host
+   * which is up, but using a bad port (i.e. not the default 8091)
+   *
+   * @pre  First the running client is shut down and a new instance
+   * is created using URIs of invalid addresses. Next an attempt is
+   * made to create the default bucket for this new instance.
+   * @post  The connection should not succeed, after which the
+   * previous cluster manager instance is restored.
+   * @throws InterruptedException the interrupted exception
+   */
+  public void testConnectionRefused() throws InterruptedException {
+    List<URI> uris = new LinkedList<URI>();
+    uris.add(URI.create("http://" + TestConfig.IPV4_ADDR + ":3454/pools"));
+    manager.shutdown();
+    manager = new ClusterManager(uris, CbTestConfig.CLUSTER_ADMINNAME,
+      CbTestConfig.CLUSTER_PASS);
+    String message = "";
+    try {
+      manager.createDefaultBucket(BucketType.COUCHBASE, 100, 0, true);
+    } catch (RuntimeException e) {
+      message = e.getMessage();
+    }
+    assertEquals("Unable to connect to cluster", message);
+    manager = getClusterManager();
+  }
+
+  /**
+   * This test is performed by having the client connect
+   * to an IP for which no valid host is assigned.
+   *
+   * @pre  First the running client is shut down and a new instance
+   * is created using URIs of invalid addresses. Next an attempt is
+   * made to create the default bucket for this new instance.
+   * @post  The connection should not succeed, after which the
+   * previous cluster manager instance is restored.
+   * @throws InterruptedException the interrupted exception
+   */
+  public void testNetworkUnreachable() throws InterruptedException {
+    List<URI> uris = new LinkedList<URI>();
+    uris.add(URI.create("http://123.123.123.123:8091/pools"));
+    manager.shutdown();
+    manager = new ClusterManager(uris, CbTestConfig.CLUSTER_ADMINNAME,
+      CbTestConfig.CLUSTER_PASS);
+    String message = "";
+    try {
+      manager.createDefaultBucket(BucketType.COUCHBASE, 100, 0, true);
+    } catch (RuntimeException e) {
+      message = e.getMessage();
+    }
+    assertEquals("Unable to connect to cluster", message);
+    manager = getClusterManager();
+  }
+  /**
    * Create and delete the default buckets with some bad server addresses.
    *
    * @pre  It first creates a list of URIs and shuts down the cluster manager.
