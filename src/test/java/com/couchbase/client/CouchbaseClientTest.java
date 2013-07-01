6e529cd097806beaa1fc7cf514b560153c054bd5
Michael Nitschinger <michael@nitschinger.at>
2013-07-01 16:05:25 +0200

    Adapting testStatsKey Testcase to account changes in 2.1.0.
    This changeset makes sure that the test case passes for both
    post 2.1.0 and pre 2.1.0, because the exposed key stats
    have changed between versions.
    
    Change-Id: Ia32dd34c5cb5b504471f4660f152e7f4799b8f66
    Reviewed-on: http://review.couchbase.org/27215
    Reviewed-by: Matt Ingenthron <matt@couchbase.com>
    Tested-by: Michael Nitschinger <michael.nitschinger@couchbase.com>

--- src/test/java/com/couchbase/client/CouchbaseClientTest.java
+++ src/test/java/com/couchbase/client/CouchbaseClientTest.java
@@ -359,6 +359,10 @@ public class CouchbaseClientTest extends BinaryClientTest {
    * @post Test asserts true if the status is success. Also it asserts
    * true if all the parameters related to the key are available.
    *
+   * <p>Note that since Couchbase Server 2.1, two stats are not exported
+   * anymore, this is why the test cases branches and checks for those
+   * two states individually.</p>
+   *
    * @throws Exception
    */
   public void testStatsKey() throws Exception {
@@ -367,18 +371,18 @@ public class CouchbaseClientTest extends BinaryClientTest {
 
     OperationFuture<Map<String, String>> future =
         ((CouchbaseClient)client).getKeyStats("key");
-    System.err.println(future);
-    System.err.println(future.getStatus());
     assertTrue(future.getStatus().isSuccess());
     Map<String, String> stats = future.get();
-    assertTrue(stats.size() == 7);
+    assertTrue(stats.size() == 5 || stats.size() == 7);
     assertTrue(stats.containsKey("key_vb_state"));
     assertTrue(stats.containsKey("key_flags"));
     assertTrue(stats.containsKey("key_is_dirty"));
     assertTrue(stats.containsKey("key_cas"));
-    assertTrue(stats.containsKey("key_data_age"));
     assertTrue(stats.containsKey("key_exptime"));
-    assertTrue(stats.containsKey("key_last_modification_time"));
+    if(stats.size() == 7) {
+      assertTrue(stats.containsKey("key_data_age"));
+      assertTrue(stats.containsKey("key_last_modification_time"));
+    }
 
     future = ((CouchbaseClient)client).getKeyStats("key1");
     assertFalse(future.getStatus().isSuccess());
