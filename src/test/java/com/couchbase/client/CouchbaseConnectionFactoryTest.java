01043e3cca69c1a781a2a26f35eb89d480a25704
Michael Nitschinger <michael@nitschinger.at>
2013-10-11 08:10:56 +0200

    Make sure random tests have better chance of succeeding.
    Change-Id: Iacc07dfd949ffaaacb64e68349580952c6c51df2
    Reviewed-on: http://review.couchbase.org/29465
    Tested-by: Michael Nitschinger <michael.nitschinger@couchbase.com>
    Reviewed-by: Matt Ingenthron <matt@couchbase.com>

--- src/test/java/com/couchbase/client/CouchbaseConnectionFactoryTest.java
+++ src/test/java/com/couchbase/client/CouchbaseConnectionFactoryTest.java
@@ -129,15 +129,23 @@ public class CouchbaseConnectionFactoryTest {
     );
 
     List<URI> oldList = connFact.getStoredBaseList();
-    int oldIndex = oldList.indexOf(new URI("http://127.0.0.1:8091/pools"));
+    int oIndex1 = oldList.indexOf(new URI("http://127.0.0.1:8091/pools"));
+    int oIndex2 = oldList.indexOf(new URI("http://127.0.0.2:8091/pools"));
+    int oIndex3 = oldList.indexOf(new URI("http://127.0.0.3:8091/pools"));
+    int oIndex4 = oldList.indexOf(new URI("http://127.0.0.5:8091/pools"));
 
-    int tries = 10;
+    int tries = 100;
     for(int i = 0; i < tries; i++) {
       connFact.updateStoredBaseList(connFact.getVBucketConfig());
       assertTrue(providerMock.baseListUpdated);
       List<URI> newList = connFact.getStoredBaseList();
-      int newIndex = newList.indexOf(new URI("http://127.0.0.1:8091/pools"));
-      if (oldIndex != newIndex) {
+      System.out.println("old: " + oldList);
+      System.out.println("new: " + newList);
+      int nIndex1 = newList.indexOf(new URI("http://127.0.0.1:8091/pools"));
+      int nIndex2 = newList.indexOf(new URI("http://127.0.0.2:8091/pools"));
+      int nIndex3 = newList.indexOf(new URI("http://127.0.0.3:8091/pools"));
+      int nIndex4 = newList.indexOf(new URI("http://127.0.0.5:8091/pools"));
+      if (oIndex1 != nIndex1 || oIndex2 != nIndex2 || oIndex3 != nIndex3 || oIndex4 == nIndex4) {
         assertTrue(true);
         return;
       }
