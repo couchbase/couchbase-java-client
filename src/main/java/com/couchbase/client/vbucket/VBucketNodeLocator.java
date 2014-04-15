23c550efa2c9e35e921562e388cdc95026b4b0b2
Michael Nitschinger <michael@nitschinger.at>
2014-04-15 06:49:45 +0200

    Fix ConcurrentModificationException in VBucketNodeLocator
    Motivation
    ----------
    If a node needs to be removed in the VBucketNodeLocator#fillNodeEntries, the previous
    code could lead to a ConcurrentModificationException.
    
    Modifications
    -------------
    Use an iterator to safely remove it from the map.
    
    Result
    ------
    No more ConcurrentModificationException.
    
    Change-Id: Icc9b86661c69436e60f9dc4ace559ecfff4ff28b
    Reviewed-on: http://review.couchbase.org/35719
    Tested-by: Michael Nitschinger <michael.nitschinger@couchbase.com>
    Reviewed-by: Brett Lawson <brett19@gmail.com>

--- src/main/java/com/couchbase/client/vbucket/VBucketNodeLocator.java
+++ src/main/java/com/couchbase/client/vbucket/VBucketNodeLocator.java
@@ -236,16 +236,21 @@ public class VBucketNodeLocator extends SpyObject implements NodeLocator {
         getLogger().debug("Node added is %s.", node);
       }
     }
+
     // Iterate over the map and check for entries not populated
-    for (Map.Entry<String, MemcachedNode> entry : vbnodesMap.entrySet()) {
+    Iterator<Map.Entry<String, MemcachedNode>> iterator =
+      vbnodesMap.entrySet().iterator();
+    while (iterator.hasNext()) {
+      Map.Entry<String, MemcachedNode> entry = iterator.next();
       if (entry.getValue() == null) {
         getLogger().error("Critical reconfiguration error: "
             + "Server list from Configuration and Nodes "
             + "are out of synch. causing %s to be removed",
-                entry.getKey());
-        vbnodesMap.remove(entry.getKey());
+          entry.getKey());
+        iterator.remove();
       }
     }
+
     return Collections.unmodifiableMap(vbnodesMap);
   }
 
