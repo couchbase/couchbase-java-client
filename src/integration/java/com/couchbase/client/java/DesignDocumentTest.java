9f625f6f84494425239864b9f4cc0c615d05c77d
Michael Nitschinger <michael@nitschinger.at>
2014-08-07 17:25:38 +0200

    Adding more design document tests.
    Change-Id: Ife751f79c03a276423dcceab09ca17e9cbe67123
    Reviewed-on: http://review.couchbase.org/40379
    Reviewed-by: Michael Nitschinger <michael.nitschinger@couchbase.com>
    Tested-by: Michael Nitschinger <michael.nitschinger@couchbase.com>

--- src/integration/java/com/couchbase/client/java/DesignDocumentTest.java
+++ src/integration/java/com/couchbase/client/java/DesignDocumentTest.java
@@ -9,9 +9,7 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.*;
 
 /**
  * Verifies the functionality of the Design Document management facilities.
@@ -111,22 +109,83 @@ public class DesignDocumentTest extends ClusterDependentTest {
 
     @Test
     public void shouldGetDesignDocuments() {
+        List<View> views = Arrays.asList(DefaultView.create("v1", "function(d,m){}"));
+        DesignDocument designDocument1 = DesignDocument.create("doc1", views);
+        DesignDocument designDocument2 = DesignDocument.create("doc3", views);
+        DesignDocument designDocument3 = DesignDocument.create("doc2", views);
 
+        manager.upsertDesignDocument(designDocument1).toBlocking().single();
+        manager.upsertDesignDocument(designDocument2).toBlocking().single();
+        manager.upsertDesignDocument(designDocument3).toBlocking().single();
+
+        List<DesignDocument> docs = manager.getDesignDocuments().toList().toBlocking().single();
+        assertTrue(docs.size() >= 3);
+        int found = 0;
+        for (DesignDocument doc : docs) {
+            if (doc.name().equals("doc1") || doc.name().equals("doc2") || doc.name().equals("doc3")) {
+                found++;
+            }
+        }
+        assertEquals(3, found);
     }
 
     @Test
     public void shouldRemoveDesignDocument() {
+        List<View> views = Arrays.asList(
+            DefaultView.create("v1", "function(d,m){}"),
+            DefaultView.create("v2", "function(d,m){}", "_count")
+        );
 
+        DesignDocument designDocument = DesignDocument.create("remove1", views);
+        manager.upsertDesignDocument(designDocument).toBlocking().single();
+
+        DesignDocument found = manager.getDesignDocument("remove1").toBlocking().single();
+        assertNotNull(found);
+        assertEquals("remove1", found.name());
+        assertEquals(2, found.views().size());
+
+        assertTrue(manager.removeDesignDocument("remove1").toBlocking().single());
+
+        found = manager.getDesignDocument("remove1").toBlocking().singleOrDefault(null);
+        assertNull(found);
     }
 
     @Test
     public void shouldPublishDesignDocument() {
+        List<View> views = Arrays.asList(
+            DefaultView.create("v1", "function(d,m){}"),
+            DefaultView.create("v2", "function(d,m){}", "_count")
+        );
 
+        DesignDocument designDocument = DesignDocument.create("pub1", views);
+        manager.upsertDesignDocument(designDocument, true).toBlocking().single();
+
+        DesignDocument found = manager.getDesignDocument("pub1").toBlocking().singleOrDefault(null);
+        assertNull(found);
+
+        manager.publishDesignDocument("pub1").toBlocking().single();
+
+        found = manager.getDesignDocument("pub1").toBlocking().single();
+        assertNotNull(found);
+        assertEquals("pub1", found.name());
+        assertEquals(2, found.views().size());
     }
 
-    @Test
+    @Test(expected = DesignDocumentAlreadyExistsException.class)
     public void shouldNotOverrideOnPublish() {
+        List<View> views = Arrays.asList(
+            DefaultView.create("v1", "function(d,m){}"),
+            DefaultView.create("v2", "function(d,m){}", "_count")
+        );
 
+        DesignDocument designDocument = DesignDocument.create("pub2", views);
+        manager.upsertDesignDocument(designDocument, true).toBlocking().single();
+
+        DesignDocument found = manager.getDesignDocument("pub2").toBlocking().singleOrDefault(null);
+        assertNull(found);
+
+        manager.publishDesignDocument("pub2").toBlocking().single();
+        manager.publishDesignDocument("pub2").toBlocking().single();
     }
 
 }
