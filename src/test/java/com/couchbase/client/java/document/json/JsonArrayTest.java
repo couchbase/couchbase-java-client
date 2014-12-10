5d424b2116faa139d4ce30e8093d0d56851103e2
Simon Baslé <simon.basle@gmail.com>
2014-12-10 19:16:02 +0100

    JCBC-651: Allow putting collections in JsonValues
    Motivation
    ----------
    We already added factory methods to JsonObject and JsonArray to create
    them from collections (respectively Map and List).
    It would be even more easy to also allow such collections to be added
    on an instance as sub elements, converting them to the corresponding
    allowed type on the fly.
    
    Modifications
    -------------
    Added put(key, Map) and put(key, List) to JsonObject.
    Added add(Map) and add(List) to JsonArray.
    All four methods transform the collection to a JsonValue using adhoc
    factory method then call the corresponding overload.
    
    Added tests to check this behavior.
    
    Result
    ------
    Users have now more options to add Maps and Lists to a JsonObject or a
    JsonArray instance, provided the Map and List conforts to limitations
    described in the factory methods. They are added as sub-JsonObject or
    sub-JsonArray and should be retrieved as such.
    
    All tests pass.
    
    Change-Id: I80e78a4a37e4c3b53242c5c0b7155b2f052fc68b
    Reviewed-on: http://review.couchbase.org/44186
    Tested-by: Simon Baslé <simon@couchbase.com>
    Reviewed-by: Michael Nitschinger <michael.nitschinger@couchbase.com>
    Reviewed-on: http://review.couchbase.org/44699
    Tested-by: Michael Nitschinger <michael.nitschinger@couchbase.com>
    Reviewed-by: Simon Baslé <simon@couchbase.com>

--- src/test/java/com/couchbase/client/java/document/json/JsonArrayTest.java
+++ src/test/java/com/couchbase/client/java/document/json/JsonArrayTest.java
@@ -33,6 +33,7 @@ import static org.junit.Assert.fail;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -233,4 +234,32 @@ public class JsonArrayTest {
         assertNull(arr.getArray(2).get(0));
     }
 
+    @Test
+    public void shouldAddMapAsAJsonObject() {
+        Map<String, Object> map = new HashMap<String, Object>(2);
+        map.put("item1", "value1");
+        map.put("item2", true);
+        JsonArray arr = JsonArray.create().add(map);
+
+        assertEquals(1, arr.size());
+        assertNotNull(arr.get(0));
+        assertTrue(arr.get(0) instanceof JsonObject);
+        assertTrue(arr.getObject(0).containsKey("item1"));
+        assertTrue(arr.getObject(0).containsKey("item2"));
+    }
+
+    @Test
+    public void shouldAddListAsAJsonArray() {
+        List<Object> list = new ArrayList<Object>(2);
+        list.add("value1");
+        list.add(true);
+        JsonArray arr = JsonArray.create().add(list);
+
+        assertEquals(1, arr.size());
+        assertNotNull(arr.get(0));
+        assertTrue(arr.get(0) instanceof JsonArray);
+        assertEquals(2, arr.getArray(0).size());
+        assertEquals("value1", arr.getArray(0).get(0));
+        assertEquals(Boolean.TRUE, arr.getArray(0).get(1));
+    }
 }
