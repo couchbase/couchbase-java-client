9f81c850578cb29fa233d9220839508ff477853a
Simon Baslé <simon.basle@gmail.com>
2014-12-22 10:29:48 +0100

    JVMCBC-663: Correctly escape Json in toString()
    Motivation
    ----------
    The Json specification indicates that control characters in Json must
    be escaped. A subset of characters have a simple escape sequence with
    an antislash (eg: \n, \b) while other must be represented in unicode 16
    bits representation \uXXXX.
    
    JsonObject and JsonArray currently don't produce escaped Json via their
    toString methods.
    
    Modifications
    -------------
    Modified JsonArray and JsonObject toString methods so that they rely on
    the underlying Jackson transcoder to produce an output that is correct
    in respect to Json escaping.
    
    Added escaping tests for special characters.
    
    Result
    ------
    Adding a String that contains special characters like a carriage return
    in a JsonObject or JsonArray now produce correctly escaped Json when
    toString is called. New tests validate that.
    
    Change-Id: I04cc7aa87c11a7b9ec28a70237230bdbb6a6936c
    Reviewed-on: http://review.couchbase.org/44574
    Tested-by: Simon Baslé <simon@couchbase.com>
    Reviewed-by: Michael Nitschinger <michael.nitschinger@couchbase.com>

--- src/main/java/com/couchbase/client/java/document/json/JsonArray.java
+++ src/main/java/com/couchbase/client/java/document/json/JsonArray.java
@@ -27,6 +27,8 @@ import java.util.List;
 import java.util.ListIterator;
 import java.util.Map;
 
+import com.couchbase.client.java.transcoder.JacksonTransformers;
+
 /**
  * Represents a JSON array that can be stored and loaded from Couchbase Server.
  *
@@ -440,31 +442,11 @@ public class JsonArray extends JsonValue implements Iterable<Object> {
      */
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("[");
-        for (int i = 0; i < content.size(); i++) {
-            Object item = content.get(i);
-            boolean isString = item instanceof String;
-
-            if (isString) {
-                sb.append("\"");
-            }
-
-            if (item == null) {
-                sb.append("null");
-            } else {
-                sb.append(item.toString());
-            }
-
-            if (isString) {
-                sb.append("\"");
-            }
-            if (i < content.size()-1) {
-                sb.append(",");
-            }
+        try {
+            return JacksonTransformers.MAPPER.writeValueAsString(this);
+        } catch (Exception e) {
+            throw new IllegalStateException("Cannot convert JsonArray to Json String", e);
         }
-        sb.append("]");
-        return sb.toString();
     }
 
     @Override
