5b5e0efb1d533ee2489e50e1cf4281436f900b8d
Michael Nitschinger <michael@nitschinger.at>
2015-02-17 18:32:20 +0100

    JCBC-713: Fix leak in flush()
    Motivation
    ----------
    The upsert response content was not properly released, this is now
    fixed.
    
    Change-Id: Ia4f3f194973c76e5156a2779309fec50c7f707d6
    Reviewed-on: http://review.couchbase.org/46951
    Tested-by: Michael Nitschinger <michael.nitschinger@couchbase.com>
    Reviewed-by: Simon Baslé <simon@couchbase.com>

--- src/main/java/com/couchbase/client/java/bucket/DefaultAsyncBucketManager.java
+++ src/main/java/com/couchbase/client/java/bucket/DefaultAsyncBucketManager.java
@@ -23,23 +23,33 @@ package com.couchbase.client.java.bucket;
 
 import com.couchbase.client.core.ClusterFacade;
 import com.couchbase.client.core.CouchbaseException;
-import com.couchbase.client.core.message.CouchbaseResponse;
 import com.couchbase.client.core.message.ResponseStatus;
+import com.couchbase.client.core.message.config.BucketConfigRequest;
+import com.couchbase.client.core.message.config.BucketConfigResponse;
+import com.couchbase.client.core.message.config.FlushRequest;
+import com.couchbase.client.core.message.config.FlushResponse;
+import com.couchbase.client.core.message.config.GetDesignDocumentsRequest;
+import com.couchbase.client.core.message.config.GetDesignDocumentsResponse;
 import com.couchbase.client.core.message.kv.GetRequest;
 import com.couchbase.client.core.message.kv.GetResponse;
 import com.couchbase.client.core.message.kv.UpsertRequest;
-import com.couchbase.client.core.message.config.*;
-import com.couchbase.client.core.message.view.*;
+import com.couchbase.client.core.message.kv.UpsertResponse;
+import com.couchbase.client.core.message.view.GetDesignDocumentRequest;
+import com.couchbase.client.core.message.view.GetDesignDocumentResponse;
+import com.couchbase.client.core.message.view.RemoveDesignDocumentRequest;
+import com.couchbase.client.core.message.view.RemoveDesignDocumentResponse;
+import com.couchbase.client.core.message.view.UpsertDesignDocumentRequest;
+import com.couchbase.client.core.message.view.UpsertDesignDocumentResponse;
 import com.couchbase.client.deps.io.netty.buffer.Unpooled;
 import com.couchbase.client.deps.io.netty.util.CharsetUtil;
 import com.couchbase.client.java.CouchbaseAsyncBucket;
 import com.couchbase.client.java.document.json.JsonArray;
 import com.couchbase.client.java.document.json.JsonObject;
+import com.couchbase.client.java.error.DesignDocumentAlreadyExistsException;
+import com.couchbase.client.java.error.DesignDocumentException;
 import com.couchbase.client.java.error.FlushDisabledException;
 import com.couchbase.client.java.error.TranscodingException;
 import com.couchbase.client.java.view.DesignDocument;
-import com.couchbase.client.java.error.DesignDocumentAlreadyExistsException;
-import com.couchbase.client.java.error.DesignDocumentException;
 import rx.Observable;
 import rx.functions.Func1;
 
@@ -91,10 +101,13 @@ public class DefaultAsyncBucketManager implements AsyncBucketManager {
     public Observable<Boolean> flush() {
         final String markerKey = "__flush_marker";
         return core
-            .send(new UpsertRequest(markerKey, Unpooled.copiedBuffer(markerKey, CharsetUtil.UTF_8), bucket))
-            .flatMap(new Func1<CouchbaseResponse, Observable<FlushResponse>>() {
+            .<UpsertResponse>send(new UpsertRequest(markerKey, Unpooled.copiedBuffer(markerKey, CharsetUtil.UTF_8), bucket))
+            .flatMap(new Func1<UpsertResponse, Observable<FlushResponse>>() {
                 @Override
-                public Observable<FlushResponse> call(CouchbaseResponse res) {
+                public Observable<FlushResponse> call(UpsertResponse res) {
+                    if (res.content() != null && res.content().refCnt() > 0) {
+                        res.content().release();
+                    }
                     return core.send(new FlushRequest(bucket, password));
                 }
             }).flatMap(new Func1<FlushResponse, Observable<? extends Boolean>>() {
