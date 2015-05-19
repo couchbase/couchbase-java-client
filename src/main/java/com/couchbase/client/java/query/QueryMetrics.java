44dedaa1aa6b6779d722f36c70802a1bf14c4f23
Michael Nitschinger <michael@nitschinger.at>
2015-05-19 13:07:44 +0200

    Add toString() to QueryMetrics.
    Change-Id: I7a61e83214829f69894b41ecb47681d789dbfa63
    Reviewed-on: http://review.couchbase.org/51211
    Tested-by: Michael Nitschinger <michael.nitschinger@couchbase.com>
    Reviewed-by: Simon Baslé <simon@couchbase.com>

--- src/main/java/com/couchbase/client/java/query/QueryMetrics.java
+++ src/main/java/com/couchbase/client/java/query/QueryMetrics.java
@@ -21,11 +21,11 @@
  */
 package com.couchbase.client.java.query;
 
-import java.io.Serializable;
-
 import com.couchbase.client.core.annotations.InterfaceAudience;
 import com.couchbase.client.core.annotations.InterfaceStability;
 import com.couchbase.client.java.document.json.JsonObject;
+
+import java.io.Serializable;
 
 /**
  * A class that represents N1QL metrics.
@@ -160,5 +160,19 @@ public class QueryMetrics implements Serializable{
      */
     public JsonObject asJsonObject() {
         return rawMetrics;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("QueryMetrics{");
+        sb.append(", resultCount=").append(resultCount);
+        sb.append(", errorCount=").append(errorCount);
+        sb.append(", warningCount=").append(warningCount);
+        sb.append(", mutationCount=").append(mutationCount);
+        sb.append(", resultSize=").append(resultSize);
+        sb.append(", elapsedTime='").append(elapsedTime).append('\'');
+        sb.append(", executionTime='").append(executionTime).append('\'');
+        sb.append('}');
+        return sb.toString();
     }
 }
