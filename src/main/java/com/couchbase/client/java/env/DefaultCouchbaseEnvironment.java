5216638c192e64bf77747901f60f49c833f21e5f
Michael Nitschinger <michael@nitschinger.at>
2015-08-19 09:28:19 +0200

    Align System -> RuntimeMetricEvent rename with core.
    Change-Id: I781576e38012b1a104d90a3c58574ab86bd0efdb
    Reviewed-on: http://review.couchbase.org/54575
    Tested-by: Michael Nitschinger <michael.nitschinger@couchbase.com>
    Reviewed-by: Simon Baslé <simon@couchbase.com>

--- src/main/java/com/couchbase/client/java/env/DefaultCouchbaseEnvironment.java
+++ src/main/java/com/couchbase/client/java/env/DefaultCouchbaseEnvironment.java
@@ -430,8 +430,8 @@ public class DefaultCouchbaseEnvironment extends DefaultCoreEnvironment implemen
         }
 
         @Override
-        public Builder systemMetricsCollectorConfig(MetricsCollectorConfig metricsCollectorConfig) {
-            super.systemMetricsCollectorConfig(metricsCollectorConfig);
+        public Builder runtimeMetricsCollectorConfig(MetricsCollectorConfig metricsCollectorConfig) {
+            super.runtimeMetricsCollectorConfig(metricsCollectorConfig);
             return this;
         }
 
