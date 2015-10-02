491d70c290c007f88f11d8d49fa7b2f7481091a9
Michael Nitschinger <michael@nitschinger.at>
2015-10-02 20:03:27 +0200

    Override environment builder for compatibility.
    Change-Id: I989a95bf8382c4d46ce31da1ea4d2b761e3ba394
    Reviewed-on: http://review.couchbase.org/55735
    Reviewed-by: Michael Nitschinger <michael.nitschinger@couchbase.com>
    Tested-by: Michael Nitschinger <michael.nitschinger@couchbase.com>

--- src/main/java/com/couchbase/client/java/env/DefaultCouchbaseEnvironment.java
+++ src/main/java/com/couchbase/client/java/env/DefaultCouchbaseEnvironment.java
@@ -439,6 +439,12 @@ public class DefaultCouchbaseEnvironment extends DefaultCoreEnvironment implemen
         }
 
         @Override
+        public Builder socketConnectTimeout(int socketConnectTimeout) {
+            super.socketConnectTimeout(socketConnectTimeout);
+            return this;
+        }
+
+        @Override
         public String packageNameAndVersion() {
             return packageNameAndVersion;
         }
