d1297651b9712b07c9adef3e830469d0a4f48a26
Simon Baslé <simon.basle@gmail.com>
2015-12-17 14:25:51 +0100

    JCBC-893: Assert KV handling of UTF16 keys
    Motivation:
    -----------
    Couchbase Server is supposed to only accept UTF8-encoded keys. The aim
    of this change is to verify that UTF16 characters in keys are correctly
    encoded and accepted by the server.
    
    Modifications:
    --------------
    Integration test with a UTF16 key added to the integration test suite.
    
    Change-Id: I707fbebcbbd4f8ec8edc442ff5fcb7d4a722f2e7
    Reviewed-on: http://review.couchbase.org/57910
    Tested-by: Simon Baslé <simon@couchbase.com>
    Reviewed-by: Sergey Avseyev <sergey.avseyev@gmail.com>

--- src/integration/java/com/couchbase/client/java/KeyValueTest.java
+++ src/integration/java/com/couchbase/client/java/KeyValueTest.java
@@ -563,4 +563,19 @@ public class KeyValueTest extends ClusterDependentTest {
         bucket().remove(JsonDocument.from(stored, stored.cas() + 1), PersistTo.MASTER);
     }
 
+    @Test
+    public void shouldStoreAndGetKeyWithUtf16Char() {
+        String key = "utf16\uD834\uDD1E"; //a nice little musical key
+        JsonDocument toStore = JsonDocument.create(key, JsonObject.create().put("UTF8", true).put("hasUTF16", true));
+
+        JsonDocument inserted = bucket().upsert(toStore);
+        JsonDocument retrieved = bucket().get(key);
+
+        assertNotNull(inserted);
+        assertNotNull(retrieved);
+        assertEquals(key, inserted.id());
+        assertEquals(key, retrieved.id());
+        assertEquals(inserted.content(), retrieved.content());
+    }
+
 }
