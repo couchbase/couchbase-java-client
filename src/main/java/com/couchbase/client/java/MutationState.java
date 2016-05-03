11791f7ab0e30c6522488aacb54401d102c9f816
Michael Nitschinger <michael@nitschinger.at>
2016-05-03 14:05:59 +0200

    JCBC-953: Add support for MutationState import.
    Motivation
    ----------
    Since exporting is supported, importing also needs to be supported.
    
    Modifications
    -------------
    This changeset implements import from both raw json string as well
    as our JsonObject. Equality has been implemented but the MutationState
    checks if the elements are contained (since ordering is not important
    and can lead to false negatives on the equality checks).
    
    Result
    ------
    Serialized MutationState can now be turned into actual objects and
    then used when querying.
    
    Change-Id: I0774962a3091c819603ffcbe7d7ed840ba20ea4a
    Reviewed-on: http://review.couchbase.org/63628
    Tested-by: Michael Nitschinger <michael.nitschinger@couchbase.com>
    Reviewed-by: Simon Baslé <simon@couchbase.com>

--- src/main/java/com/couchbase/client/java/MutationState.java
+++ src/main/java/com/couchbase/client/java/MutationState.java
@@ -163,4 +163,59 @@ public class MutationState implements Iterable<MutationToken> {
         }
         return result;
     }
+
+    /**
+     * Create a {@link MutationState} from the serialized state.
+     *
+     * @param source the source state, serialized.
+     * @return the created {@link MutationState}.
+     */
+    public static MutationState from(String source) {
+        return from(JsonObject.fromJson(source));
+    }
+
+    /**
+     * Create a {@link MutationState} from the serialized state.
+     *
+     * @param source the source state, serialized.
+     * @return the created {@link MutationState}.
+     */
+    public static MutationState from(JsonObject source) {
+        try {
+            MutationState state = new MutationState();
+            for (String bucketName : source.getNames()) {
+                JsonObject bucket = source.getObject(bucketName);
+                for (String vbid : bucket.getNames()) {
+                    JsonArray values = bucket.getArray(vbid);
+                    state.addToken(new MutationToken(
+                        Long.parseLong(vbid),
+                        Long.parseLong(values.getString(1)),
+                        values.getLong(0),
+                        bucketName
+                    ));
+                }
+            }
+            return state;
+        } catch (Exception ex) {
+            throw new IllegalStateException("Could not import MutationState from JSON.", ex);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MutationState state = (MutationState) o;
+        return tokens.containsAll(state.tokens) && state.tokens.containsAll(tokens);
+    }
+
+    @Override
+    public int hashCode() {
+        return tokens.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "MutationState{tokens=" + tokens + '}';
+    }
 }
--- src/test/java/com/couchbase/client/java/MutationStateTest.java
+++ src/test/java/com/couchbase/client/java/MutationStateTest.java
@@ -237,4 +237,50 @@ public class MutationStateTest {
         assertEquals(1, count);
     }
 
+    @Test
+    public void shouldImportFromString() {
+        String input = "{\n" +
+            "  \"default\": {\n" +
+            "    \"1\": [1, \"1234\"]\n" +
+            "  },\n" +
+            "  \"beer-sample\": {\n" +
+            "    \"25\": [10, \"5678\"]\n" +
+            "  }\n" +
+            "}";
+
+        MutationState state = MutationState.from(input);
+        int count = 0;
+        for (MutationToken token : state) {
+            count++;
+            if (token.bucket().equals("default")) {
+                assertEquals(1, token.vbucketID());
+                assertEquals(1, token.sequenceNumber());
+                assertEquals(1234, token.vbucketUUID());
+            } else if (token.bucket().equals("beer-sample")) {
+                assertEquals(25, token.vbucketID());
+                assertEquals(10, token.sequenceNumber());
+                assertEquals(5678, token.vbucketUUID());
+            } else {
+                assertTrue("Found not expected bucket name when importing MutationState", false);
+            }
+        }
+        assertEquals(2, count);
+    }
+
+    @Test
+    public void shouldImportFromJsonObject() {
+        MutationToken token = new MutationToken(1, 1234, 5678, "bucket1");
+        JsonDocument document = JsonDocument.create("id", 0, JsonObject.empty(), 0, token);
+
+        MutationState state = MutationState.from(document);
+
+        MutationToken token2 = new MutationToken(2, 8888, 9999, "bucket2");
+        JsonDocument document2 = JsonDocument.create("id", 0, JsonObject.empty(), 0, token2);
+
+        state.add(document2);
+
+        JsonObject exported = state.export();
+        assertEquals(state, MutationState.from(exported));
+    }
+
 }
