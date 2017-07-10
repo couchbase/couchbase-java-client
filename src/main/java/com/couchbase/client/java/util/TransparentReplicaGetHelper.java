4e660b618e5bc7451fad4f24fa2bb2ace1affaca
Michael Nitschinger <michael@nitschinger.at>
2017-07-10 12:42:41 +0200

    JCBC-1110: Add Experimental TransparentReplicaGetHelper
    Motivation
    ----------
    Users sometimes need assistance with transparently failing
    over to a replica get when the primary get fails. This little
    helper class provides utility methods to help with exactly that.
    
    Modifications
    -------------
    This changeset introduces a helper class which first performs
    a regular kv get and if that one fails it fails over to a
    replica get and grabs the first result it gets back. This is
    the most common situation users are using both commands together.
    
    Result
    ------
    Transparent get followed potentially by replica get is a common
    use case and now covered by a simple helper class.
    
    Change-Id: Ib1aa0f0276147977fe81bcfefb92f80f7906cc14
    Reviewed-on: http://review.couchbase.org/80443
    Tested-by: Build Bot <build@couchbase.com>
    Tested-by: Michael Nitschinger <michael@nitschinger.at>
    Reviewed-by: David Nault <david.nault@couchbase.com>

---
+++ src/main/java/com/couchbase/client/java/util/TransparentReplicaGetHelper.java
@@ -0,0 +1,163 @@
+/**
+ * Copyright (c) 2017 Couchbase, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.couchbase.client.java.util;
+
+import com.couchbase.client.core.annotations.InterfaceAudience;
+import com.couchbase.client.core.annotations.InterfaceStability;
+import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.ReplicaMode;
+import com.couchbase.client.java.document.Document;
+import com.couchbase.client.java.document.JsonDocument;
+import rx.Observable;
+import rx.Single;
+import rx.functions.Func1;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * The {@link TransparentReplicaGetHelper} abstracts common logic to first grab the
+ * active document and if that fails tries all available replicas and returns the first
+ * result.
+ *
+ * NOTE: Using these APIs is eventually consistent meaning that you cannot rely on
+ * a previous successful mutation to a document be reflected in the result. Use this
+ * API only if you favor availability over consistency on the read path.
+ */
+@InterfaceStability.Experimental
+@InterfaceAudience.Public
+public class TransparentReplicaGetHelper {
+
+    /**
+     * Asynchronously fetch the document from the primary and if that operations fails try
+     * all the replicas and return the first document that comes back from them (with custom
+     * primary and replica timeout values).
+     *
+     * @param id the document ID to fetch.
+     * @param bucket the bucket to use when fetching the doc.
+     * @param primaryTimeout the timeout to use in MS when fetching the primary.
+     * @param replicaTimeout the timeout to use in MS when subsequently fetching the replicas and primary.
+     * @return an {@link Single} with either 0 or 1 {@link JsonDocument}.
+     */
+    @InterfaceStability.Experimental
+    @InterfaceAudience.Public
+    public Single<JsonDocument> getFirstPrimaryOrReplica(final String id,
+        final Bucket bucket, final long primaryTimeout, final long replicaTimeout) {
+        return getFirstPrimaryOrReplica(id, JsonDocument.class, bucket, primaryTimeout, replicaTimeout);
+    }
+
+    /**
+     * Asynchronously fetch the document from the primary and if that operations fails try
+     * all the replicas and return the first document that comes back from them (with a custom
+     * timeout value applied to both primary and replica).
+     *
+     * @param id the document ID to fetch.
+     * @param bucket the bucket to use when fetching the doc.
+     * @param timeout the timeout to use for both primary and replica fetches (separately)
+     * @return an {@link Single} with either 0 or 1 {@link JsonDocument}.
+     */
+    @InterfaceStability.Experimental
+    @InterfaceAudience.Public
+    public Single<JsonDocument> getFirstPrimaryOrReplica(final String id,
+        final Bucket bucket, final long timeout) {
+        return getFirstPrimaryOrReplica(id, bucket, timeout, timeout);
+    }
+
+    /**
+     * Asynchronously fetch the document from the primary and if that operations fails try
+     * all the replicas and return the first document that comes back from them (using the
+     * environments KV timeout for both primary and replica).
+     *
+     *
+     * @param id the document ID to fetch.
+     * @param bucket the bucket to use when fetching the doc.
+     * @return an {@link Single} with either 0 or 1 {@link JsonDocument}.
+     */
+    @InterfaceStability.Experimental
+    @InterfaceAudience.Public
+    public Single<JsonDocument> getFirstPrimaryOrReplica(final String id, final Bucket bucket) {
+        return getFirstPrimaryOrReplica(id, bucket, bucket.environment().kvTimeout());
+    }
+
+    /**
+     * Asynchronously fetch the document from the primary and if that operations fails try
+     * all the replicas and return the first document that comes back from them (using the
+     * environments KV timeout for both primary and replica).
+     *
+     * @param id the document ID to fetch.
+     * @param target the custom document type to use.
+     * @param bucket the bucket to use when fetching the doc.
+     * @return @return a {@link Single} with either 0 or 1 {@link Document}.
+     */
+    @InterfaceStability.Experimental
+    @InterfaceAudience.Public
+    public static <D extends Document<?>> Single<D> getFirstPrimaryOrReplica(final String id,
+        final Class<D> target, final Bucket bucket) {
+        return getFirstPrimaryOrReplica(id, target, bucket, bucket.environment().kvTimeout());
+    }
+
+    /**
+     * Asynchronously fetch the document from the primary and if that operations fails try
+     * all the replicas and return the first document that comes back from them (with a custom
+     * timeout value applied to both primary and replica).
+     *
+     * @param id the document ID to fetch.
+     * @param target the custom document type to use.
+     * @param bucket the bucket to use when fetching the doc.
+     * @param timeout the timeout to use for both primary and replica fetches (separately)
+     * @return @return a {@link Single} with either 0 or 1 {@link Document}.
+     */
+    @InterfaceStability.Experimental
+    @InterfaceAudience.Public
+    public static <D extends Document<?>> Single<D> getFirstPrimaryOrReplica(final String id,
+        final Class<D> target, final Bucket bucket, final long timeout) {
+        return getFirstPrimaryOrReplica(id, target, bucket, timeout, timeout);
+    }
+
+    /**
+     * Asynchronously fetch the document from the primary and if that operations fails try
+     * all the replicas and return the first document that comes back from them.
+     *
+     * @param id the document ID to fetch.
+     * @param target the custom document type to use.
+     * @param bucket the bucket to use when fetching the doc.
+     * @param primaryTimeout the timeout to use in MS when fetching the primary.
+     * @param replicaTimeout the timeout to use in MS when subsequently fetching the replicas and primary.
+     * @return @return a {@link Single} with either 0 or 1 {@link Document}.
+     */
+    @InterfaceStability.Experimental
+    @InterfaceAudience.Public
+    public static <D extends Document<?>> Single<D> getFirstPrimaryOrReplica(final String id,
+        final Class<D> target, final Bucket bucket, final long primaryTimeout, final long replicaTimeout) {
+        Observable<D> fallback = bucket
+            .async()
+            .getFromReplica(id, ReplicaMode.ALL, target)
+            .timeout(replicaTimeout, TimeUnit.MILLISECONDS)
+            .firstOrDefault(null)
+            .filter(new Func1<D, Boolean>() {
+                @Override
+                public Boolean call(D d) {
+                    return d != null;
+                }
+            });
+
+        return bucket
+            .async()
+            .get(id, target)
+            .timeout(primaryTimeout, TimeUnit.MILLISECONDS)
+            .onErrorResumeNext(fallback)
+            .toSingle();
+    }
+}
