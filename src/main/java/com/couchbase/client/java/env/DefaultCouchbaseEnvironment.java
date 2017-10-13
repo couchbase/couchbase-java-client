e7acd348a85bafffb4c1c9b6a158f82946ed04f3
Michael Nitschinger <michael@nitschinger.at>
2017-10-13 13:27:53 +0200

    Add sslTruststore overrides
    Change-Id: Ia8595d3ab880558667ecd40a343e4867de588142
    Reviewed-on: http://review.couchbase.org/84365
    Tested-by: Michael Nitschinger <michael@nitschinger.at>
    Reviewed-by: David Nault <david.nault@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

--- src/main/java/com/couchbase/client/java/env/DefaultCouchbaseEnvironment.java
+++ src/main/java/com/couchbase/client/java/env/DefaultCouchbaseEnvironment.java
@@ -595,6 +595,24 @@ public class DefaultCouchbaseEnvironment extends DefaultCoreEnvironment implemen
         }
 
         @Override
+        public Builder sslTruststoreFile(String sslTruststoreFile) {
+            super.sslTruststoreFile(sslTruststoreFile);
+            return this;
+        }
+
+        @Override
+        public Builder sslTruststorePassword(String sslTruststorePassword) {
+            super.sslTruststorePassword(sslTruststorePassword);
+            return this;
+        }
+
+        @Override
+        public Builder sslTruststore(KeyStore sslTruststore) {
+            super.sslTruststore(sslTruststore);
+            return this;
+        }
+
+        @Override
         public DefaultCouchbaseEnvironment build() {
             return new DefaultCouchbaseEnvironment(this);
         }
