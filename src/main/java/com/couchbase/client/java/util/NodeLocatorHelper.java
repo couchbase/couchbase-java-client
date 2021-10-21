8df11baab24e4f5cdadeb7d527fa24c0001ae80a
Michael Nitschinger <michael@nitschinger.at>
2021-10-21 16:32:21 +0200

    JCBC-1886: Allow to list the number of currently available replicas for a document ID.
    Change-Id: I669bc4c1e11b92d4577cd4e177cbb8ff4ca79cc2
    Reviewed-on: http://review.couchbase.org/c/couchbase-java-client/+/164078
    Tested-by: Michael Nitschinger <michael.nitschinger@couchbase.com>
    Reviewed-by: Michael Nitschinger <michael.nitschinger@couchbase.com>

--- src/main/java/com/couchbase/client/java/util/NodeLocatorHelper.java
+++ src/main/java/com/couchbase/client/java/util/NodeLocatorHelper.java
@@ -54,18 +54,17 @@ import java.util.zip.CRC32;
 @InterfaceAudience.Public
 public class NodeLocatorHelper {
 
-    private final ConfigurationProvider configProvider;
     private final AtomicReference<BucketConfig> bucketConfig;
 
     private NodeLocatorHelper(final Bucket bucket) {
-        configProvider = bucket
-            .core()
-            .<GetConfigProviderResponse>send(new GetConfigProviderRequest())
-            .toBlocking()
-            .single()
-            .provider();
+        ConfigurationProvider configProvider = bucket
+          .core()
+          .<GetConfigProviderResponse>send(new GetConfigProviderRequest())
+          .toBlocking()
+          .single()
+          .provider();
 
-        bucketConfig = new AtomicReference<BucketConfig>(configProvider.config().bucketConfig(bucket.name()));
+        bucketConfig = new AtomicReference<>(configProvider.config().bucketConfig(bucket.name()));
 
         configProvider
             .configs()
@@ -121,9 +120,34 @@ public class NodeLocatorHelper {
 
         if (config instanceof CouchbaseBucketConfig) {
             CouchbaseBucketConfig cbc = (CouchbaseBucketConfig) config;
-            List<InetAddress> replicas = new ArrayList<InetAddress>();
+            List<InetAddress> replicas = new ArrayList<>();
             for (int i = 1; i <= cbc.numberOfReplicas(); i++) {
                 replicas.add(replicaNodeForId(id, i));
+            }
+            return replicas;
+        } else {
+            throw new UnsupportedOperationException("Bucket type not supported: " + config.getClass().getName());
+        }
+    }
+
+    /**
+     * Returns all target replica nodes {@link InetAddress} which are currently available on the bucket.
+     *
+     * @param id the document ID to check.
+     * @return the list of nodes for the given document ID.
+     */
+    public List<InetAddress> availableReplicaNodesForId(final String id) {
+        BucketConfig config = bucketConfig.get();
+
+        if (config instanceof CouchbaseBucketConfig) {
+            CouchbaseBucketConfig cbc = (CouchbaseBucketConfig) config;
+            List<InetAddress> replicas = new ArrayList<>();
+            for (int i = 1; i <= cbc.numberOfReplicas(); i++) {
+                try {
+                    replicas.add(replicaNodeForId(id, i));
+                } catch (IllegalStateException ex) {
+                    // We ignore -1 and -2 vbuckets on purpose.
+                }
             }
             return replicas;
         } else {
@@ -171,7 +195,7 @@ public class NodeLocatorHelper {
      * @return all currently known nodes.
      */
     public List<InetAddress> nodes() {
-        List<InetAddress> allNodes = new ArrayList<InetAddress>();
+        List<InetAddress> allNodes = new ArrayList<>();
         BucketConfig config = bucketConfig.get();
         for (NodeInfo nodeInfo : config.nodes()) {
             try {
